d1218cc925631a4b0fa00d8ee4e77d0f4609c1b6
Anis <anis.kraiem1996@gmail.com>
2021-11-28 15:46:53 +0100

    .

---
+++ src/app/list-products-component/list-products-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-products-component',
+  templateUrl: './list-products-component.component.html',
+  styleUrls: ['./list-products-component.component.css']
+})
+export class ListProductsComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
